2238ce29474571a7430a37e9d474812fa0257dc8
devDesu <>
2024-05-10 11:26:49 +0200

    fix: remove import.meta.url

--- web/debridge_external_call.js
+++ web/debridge_external_call.js
@@ -1022,7 +1022,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('debridge_external_call_bg.wasm', import.meta.url);
+        input = new URL('debridge_external_call_bg.wasm');
     }
     const imports = __wbg_get_imports();
 
